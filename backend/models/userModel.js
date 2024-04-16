54dff31c65d66a33be72492156297e8be69bfee8
ilovemaltesers <136581958+ilovemaltesers@users.noreply.github.com>
2024-04-16 13:37:34 +0200

    added mongoose unique validator to userModel

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,11 +1,7 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
   email: {
     type: String,
     required: true,
@@ -17,6 +13,7 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-const User = mongoose.model("User", userSchema);
+// userschema plugin
+userSchema.plugin(uniqueValidator);
 
-module.exports = User;
+module.exports = mongoose.model("User", userSchema);
