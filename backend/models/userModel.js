c09c303fe218ffd5dceeba018eefc20b9d567592
ilovemaltesers <136581958+ilovemaltesers@users.noreply.github.com>
2024-04-17 15:15:35 +0200

    fixed error uniqueValidator

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const validator = require("validator");
+const uniqueValidator = require("mongoose-unique-validator"); // Corrected import
 
 const userSchema = new mongoose.Schema({
   email: {
