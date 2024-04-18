40f4fe2e402c3e404044e1b5003487722c288282
ilovemaltesers <136581958+ilovemaltesers@users.noreply.github.com>
2024-04-18 17:29:19 +0200

    installed cors from npm and changed code to install

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,15 @@
 // password 1cITxDC2qg6LqD3K
 
 const express = require("express");
+
+//install cors
+
+const cors = require("cors");
+
 const app = express();
+
+app.use(cors());
+
 app.use(express.json());
 
 const mongoose = require("mongoose");
@@ -24,19 +32,6 @@ mongoose
     console.log("Unable to connect to MongoDB Atlas!");
     console.error(error);
   });
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
-  next();
-});
 
 app.use((req, res) => {
   res.json({ message: "Your request was successful!" });
