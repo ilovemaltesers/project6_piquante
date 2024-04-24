0878cb5766dd21d3e794b9891b500ab9803e3b8a
ilovemaltesers <136581958+ilovemaltesers@users.noreply.github.com>
2024-04-24 16:14:18 +0200

    added Multer to sauce routes file

--- backend/app.js
+++ backend/app.js
@@ -4,14 +4,14 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const auth = require("./middleware/authMiddleware");
+
 const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(auth);
+
 app.use("/api/auth", userRoutes);
 
 // MONGOOSE CONNECTION
--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-// insertion des middleware
+// insertion des middlewares
 const auth = require("../middleware/auth");
+const multer = require("../middleware/multer-config");
+
+// later to add auth and mult in the routes!!!!!
 
 module.exports = router;
