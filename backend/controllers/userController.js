c95ae43642bcdc7bbd7802fa0f309be03463df16
ilovemaltesers <136581958+ilovemaltesers@users.noreply.github.com>
2024-04-29 19:23:05 +0200

    fixed error bcrypt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,24 +5,31 @@ const jwt = require("jsonwebtoken");
 // signup function
 
 exports.signup = (req, res, next) => {
-  bcrypt.hash(req.body.password, 10).then((hash) => {
-    const user = new User({
-      email: req.body.email,
-      password: hash,
-    });
-    user
-      .save()
-      .then(() => {
-        res.status(201).json({
-          message: "User added successfully!",
-        });
-      })
-      .catch((error) => {
-        res.status(500).json({
-          error: error,
-        });
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) => {
+      const user = new User({
+        email: req.body.email,
+        password: hash,
       });
-  });
+      user
+        .save()
+        .then(() => {
+          res.status(201).json({
+            message: "User added successfully!",
+          });
+        })
+        .catch((error) => {
+          res.status(500).json({
+            error: error,
+          });
+        });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        error: error,
+      });
+    });
 };
 
 // login function
