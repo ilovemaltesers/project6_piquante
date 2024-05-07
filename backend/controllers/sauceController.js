630fdeac25ed8e183ce5414ecfd263789bba535e
ilovemaltesers <136581958+ilovemaltesers@users.noreply.github.com>
2024-05-07 11:29:14 +0200

    getAllSauces controller function working

--- backend/controllers/sauceController.js
+++ backend/controllers/sauceController.js
@@ -1,36 +1,15 @@
 const Sauce = require("../models/Sauce");
 
 exports.getAllSauces = (req, res) => {
-  res.send([
-    {
-      userId: "33",
-      name: "Cholula",
-      manufacturer: "String — manufacturer of the sauce.",
-      description:
-        "Mexican hot sauce: smooth, tangy, spicy. Made with pequin peppers.",
-      mainPepper: "Pequin Pepper",
-      imageUrl: "http://localhost:3000/images/cholula.jpeg",
-      heat: 2,
-      likes: 4243432,
-      dislikes: 23,
-      usersLiked: ["22"],
-      usersDisliked: ["23"],
-    },
-    {
-      userId: "33",
-      name: "Big Bang",
-      manufacturer: "Mr Pic's",
-      description:
-        "mélange secret des variétés de piment les plus piquantes au monde.",
-      mainPepper: "Carolina Reaper, Magma® et Judgment Day®",
-      imageUrl: "http://localhost:3000/images/bigbang.jpg",
-      heat: 10,
-      likes: 56,
-      dislikes: 7,
-      usersLiked: ["22"],
-      usersDisliked: ["23"],
-    },
-  ]);
+  Sauce.find()
+    .then((sauces) => {
+      res.status(200).json(sauces);
+    })
+    .catch((error) => {
+      res.status(400).json({
+        message: error.message,
+      });
+    });
 };
 
 exports.createSauce = (req, res) => {
