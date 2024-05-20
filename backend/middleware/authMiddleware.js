31a9c3e0e91b9e54abb8fa3295a8139027f52001
ilovemaltesers <136581958+ilovemaltesers@users.noreply.github.com>
2024-05-20 10:55:37 +0200

    updated annotations in authMiddleware file

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,11 +2,20 @@ const jwt = require("jsonwebtoken");
 
 function authorise(req, res, next) {
   try {
-    const token = req.headers.authorization.split(" ")[1]; // get the token from the Authorization header
-    const decodedToken = jwt.verify(token, "RANDOM_TOKEN_SECRET"); // verify the token
-    const userId = decodedToken.userId; // extract the user ID from the token
+    // Once the user is authenticated on the frontend,
+    // the frontend sends a bearer token with each request.
+    // To authorise routes in the middleware,
+    // we extract the bearer token from the request headers.
+    const token = req.headers.authorization.split(" ")[1];
+
+    // Once we have the token from the frontend, we verify that the user is authorised.
+    // Note: The frontend only receives the token and never generates it.
+    // The backend sends the token to the frontend, and the frontend includes the token in the headers of each request.
+    const decodedToken = jwt.verify(token, "RANDOM_TOKEN_SECRET"); // Verify the token
+    const userId = decodedToken.userId; // Extract the user ID from the token
+
+    // Check if the user ID from the token matches the user ID in the request body.
     if (req.body.userId && req.body.userId !== userId) {
-      // check if the user ID extracted from the token is different from the one sent in the request
       throw "Invalid user ID";
     } else {
       next();
@@ -17,4 +26,5 @@ function authorise(req, res, next) {
     });
   }
 }
+
 module.exports = authorise;
